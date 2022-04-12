f915f7f474bb7ac7cf246cd6fcdd605edde1ec2a
DHefner415 <swurv415@gmail.com>
2022-04-12 08:28:23 -0700

    axiosWithAuth built

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
+//Task List:
+//1. Complete axiosWithAuth
+
 const axiosWithAuth = ()=> {
-    
+    const token = localStorage.getItem('token')
+    return axios.create({
+        headers: {
+            Authorization: token,
+        },
+        baseURL:'http://localhost:9000'
+    })
 }
 
 export default axiosWithAuth;
-
-//Task List:
-//1. Complete axiosWithAuth
