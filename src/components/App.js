f84ab37d9763d202501cea2fa457a4a74de05170
DHefner415 <swurv415@gmail.com>
2022-04-12 10:39:56 -0700

    login complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,46 @@
-import React from 'react';
-import { Route } from "react-router-dom";
-import styled from 'styled-components';
-
-import Header from './Header';
-import BloomHeader from './BloomHeader';
-import Login from './Login';
-
-const App = () => {
-  return (
-    <AppContainer>
-      <BloomHeader/>
-      <Header/>
-      <RouteContainer>
-        <Route exact path="/">
-          <Login/>
-        </Route>          
-      </RouteContainer>
-    </AppContainer>
-  )
-}
-
-export default App;
-
+import React from 'react'
+import { Route } from 'react-router-dom'
+import styled from 'styled-components'
 //Task List
 //1. Create and import PrivateRoute component.
 //2. Create a Route for Login pointing to '/login.'
 //3. Create a PrivateRoute for View component point to '/view.'
 //4. Create a PrivateRoute for Logout component pointing to '/logout.'
 
+// import PrivateRoute from './PrivateRoute'
+import Header from './Header'
+import BloomHeader from './BloomHeader'
+import Login from './Login'
+import View from './View'
+import Logout from './Logout'
+
+const App = () => {
+  return (
+    <AppContainer>
+      <BloomHeader />
+      <Header />
+      <RouteContainer>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+
+        <Route path='/login'>
+          <Login />
+        </Route>
+
+        <Route route='/logout'>
+          <Logout />
+        </Route>
+
+        <Route path='/view'>
+          <View />
+        </Route>
+      </RouteContainer>
+    </AppContainer>
+  )
+}
+
+export default App
 
 const AppContainer = styled.div`
   height: 100%;
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,23 @@
-import React from "react";
-import { Route } from "react-router-dom";
-
-function PrivateRoute() {
-  return (<Route/>);
-}
-
-export default PrivateRoute;
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
 
 //Task List:
 //1. Complete PrivateRoute
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (localStorage.getItem('token')) {
+          return <Component {...props} />
+        } else {
+          return <Redirect to='/login' />
+        }
+      }}
+    />
+  )
+}
+
+export default PrivateRoute
+
